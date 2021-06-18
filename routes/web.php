dd74d872dd4c75b686ba7de9a18f67ab97bdae34
civanisa <civanisaa@gmail.com>
2021-06-18 11:53:22 +0300

    update 1

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Ornek;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ornek', function() {
+    return view('ornek');
+});
+
+Route::get('/phpsivas/{isim}', [Ornek::class,'test']);
